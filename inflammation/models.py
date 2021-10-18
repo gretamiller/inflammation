c45511999aaf65764e7e4485c523833cd29dad8a
gretamiller <gretaamymiller@gmail.com>
2021-10-18 12:31:32 +0100

    testing for normalise function

--- inflammation/models.py
+++ inflammation/models.py
@@ -8,6 +8,21 @@ import inflammation
 import os
 import inspect
 
+def patient_normalise(data):
+    """
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    Any negative values are clipped to 0
+    """
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
+
 def get_data_dir():
     """get default directory holding data files"""
     return os.path.dirname(inspect.getfile(inflammation)) + '/data'
